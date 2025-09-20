46bb6e926875154f00609830d84dcaa84a0bf949
scumsuck <tempural@gmail.com>
2025-09-20 02:42:15 -0400

    guestbook

---
+++ src/siteConfig.ts
@@ -0,0 +1,13 @@
+import type {
+  SiteConfiguration,
+
+} from "../types.ts";
+
+export const SITE: SiteConfiguration = {
+  title: "Barebones",
+  description:
+    "A barebones starter theme. Built with Astro, Tailwind CSS, and Markdown.",
+  href: "https://barebones.superwebthemes.com",
+  author: "SuperWeb Development Inc.",
+  locale: "en-CA",
+};
