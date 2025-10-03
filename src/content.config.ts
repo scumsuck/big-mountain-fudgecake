58b92c00ba28e18e86e69d8029c93a7ccc493f47
scumsuck <tempural@gmail.com>
2025-10-03 01:24:55 -0400

    content config fix plx

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,16 +1,18 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from "astro/loaders";
 
 const art = defineCollection({
-  loader: glob({ pattern: "**/*.md", base: "./src/content/art" }),
-  schema: z.object({
+    loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/art" }),
+    schema: z.object({
     title: z.string(),
     description: z.string(),
     pubDate: z.coerce.date(),
-    img: image().refine(() => true).optional(),
-    imgAlt: z.string(),
+      image: z.object({
+        url: z.string(),
+        alt: z.string()
+      }),
     tags: z.array(z.string())   
-  })
+    })
 });
+// Export a single `collections` object to register your collection(s)
 
 export const collections = { art };
