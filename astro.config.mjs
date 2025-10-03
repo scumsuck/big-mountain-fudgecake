0b0672b3180f21bb162660d59649dd9d42ee5d22
scumsuck <tempural@gmail.com>
2025-10-03 01:43:55 -0400

    plz fix content.config.ts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from "@astrojs/react";
 
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://fudgecak.scumsuck.com",
-  integrations: [react()]
+    devToolbar: {
+    enabled: false,
+  },
+    integrations: [react()],
+  prefetch: true,
+  vite: {
+    ssr: {
+      noExternal: ["smartypants"],
+    },
+  },
 });
