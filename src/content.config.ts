cc3c8f8e3146b1c4965b125cc225f64c7c32e7dd
scumsuck <tempural@gmail.com>
2025-10-03 01:33:36 -0400

    plz fix content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,1 @@
 import { defineCollection, z } from "astro:content";
-
-const art = defineCollection({
-    loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/art" }),
-    schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
-    tags: z.array(z.string())   
-    })
-});
-// Export a single `collections` object to register your collection(s)
-
-export const collections = { art };
